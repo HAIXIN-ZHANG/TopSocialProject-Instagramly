a93ef2771463e00fa3422ec28becb43bdf8dbfdc
HAIXIN ZHANG <haixin.zhang@go1.com>
2024-10-16 16:24:13 +0800

    update code final

--- server/index.js
+++ server/index.js
@@ -8,12 +8,12 @@ import { handleErrors } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 // 增加 JSON 和 URL 编码请求体大小限制
 app.use(bodyParser.json({ limit: "10mb" })); // 例如，10MB
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
-
-app.use(cors());
-app.use(express.json());
 
 app.use("/api", router);
 //use error middleware at the end
