0ebbbb79d3d5cca98a5dd07df9487999b9ca22d0
HAIXIN ZHANG <haixin.zhang@go1.com>
2024-10-16 16:19:20 +0800

    update code final

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import path from "path-browserify";
 export default defineConfig({
   server: {
     port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   resolve: {
     alias: {
